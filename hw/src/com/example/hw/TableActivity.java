b32992b4b22856a8f938a4d5cdc7ddd2d05e743c
yeqingyang <lpz8120903@163.com>
2014-06-03 19:01:49 +0800

    234

---
+++ hw/src/com/example/hw/TableActivity.java
@@ -0,0 +1,11 @@
+package com.example.hw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableActivity extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.table);
+	}
+}
